d15fb305235c495ec7c8978c55ad60e3b5d1d3f5
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-10-09 22:22:10 -0400

    lots of changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -16,13 +18,58 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        int slots = 4;
+        int removedModel;
+
+        ArrayDeque<Integer> bumpLine = new ArrayDeque<>(slots);
+
+        HashMap<Character, Integer> showRoom = new HashMap<>();
+        // data structure for showroom?
+
+        showRoom.put('a', 0);
+        showRoom.put('b', 0);
+        showRoom.put('c', 0);
+        showRoom.put('d', 0);
+
         Scanner requestReader = null;
         try {
             requestReader = new Scanner( new File("customerRequests.txt"));
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        int request = requestReader.nextInt();
-        System.out.println(request);
+        while (requestReader.hasNextInt()) {
+            System.out.println("getting customer request...");
+
+            int request = requestReader.nextInt();
+            System.out.println("request: model " + request);
+
+            // check showroom for request
+            System.out.println("checking showroom for model " + request);
+
+            if (showRoom.containsValue(request)) {
+                // if in showroom, log 'Hit'
+                System.out.println("model " + request + " is in the showroom. Yay! It's a HIT!");
+            } else {
+                // if not in showroom, log 'Fail'
+                System.out.println("model " + request + " is NOT in the showroom. epic FAIL!");
+                // who's next in line to get bumped
+                if (bumpLine.peek() != null) {
+                    System.out.println("pulling next model from FIFO queue...");
+                    removedModel = bumpLine.remove();
+                    System.out.println("bumped model " + removedModel + " from FIFO queue.");
+                    System.out.println("getting showroom slot for model " + removedModel + ".");
+                    // obtain showroom slot
+                    System.out.println("replacing model " + removedModel + " with model " + request + ".");
+                    // remove model from showroom.
+                }
+                // add request to queue, "pull car from back-lot" and add to showroom
+                System.out.println("adding model " + request + " to showroom...");
+                // add requested model to showroom here
+                System.out.println("adding model " + request + " to back of FIFO queue...");
+                bumpLine.add(request);
+                System.out.println("bumpLine: " + bumpLine.toString());
+                System.out.println("Showroom: " + showRoom.values());
+            }
+        }
     }
 }
